1f834f65316a522e8fa03cc9a8338c077f7e5877
Devanshu <devanshupanigrahi2004@gmail.com>
2023-04-25 15:08:18 +0530

    Added Download button

--- app.js
+++ app.js
@@ -37,13 +37,28 @@ const getImages = async () => {
             cardTitle.innerText = inputElement.value;
             const cardFooter = document.createElement('div');
             cardFooter.classList.add('px-6', 'py-4', 'text-center');
-            const buttonElement = document.createElement('button');
-            buttonElement.classList.add('bg-green-500','text-3xl','hover:bg-red-900', 'text-white','border-lg','border-green-700','hover:text-black', 'font-bold', 'py-2', 'px-4', 'rounded','italic','font-serif' , 'focus:outline-none', 'focus:shadow-outline');
-            buttonElement.innerText = 'View';
-            buttonElement.addEventListener('click', () => {
+            const viewButtonElement = document.createElement('button');
+            viewButtonElement.classList.add('bg-white','text-lg','hover:bg-green-500', 'text-green-500','border-2','border-green-500','hover:text-white', 'mx-2','font-bold', 'py-2', 'px-4', 'rounded','italic','font-serif' , 'focus:outline-none', 'focus:shadow-outline');
+            viewButtonElement.innerText = 'View';
+            viewButtonElement.addEventListener('click', () => {
                 window.open(imageObject.url, '_blank');
             });
-            cardFooter.appendChild(buttonElement);
+            const downloadButtonElement = document.createElement('button');
+            downloadButtonElement.classList.add('bg-white','text-lg','hover:bg-yellow-500', 'text-yellow-500','border-2','border-yellow-500','hover:text-black', 'font-bold', 'mx-2', 'py-2', 'px-4', 'rounded','italic','font-serif' , 'focus:outline-none', 'focus:shadow-outline');
+            downloadButtonElement.innerText = 'Download';
+            downloadButtonElement.addEventListener('click', () => {
+                const link = document.createElement('a');
+                link.href = imageObject.url;
+                link.download = `${inputElement.value}_${Date.now()}.jpg`;
+                link.target = '_blank';
+                document.body.appendChild(link);
+                setTimeout(() => {
+                    link.click();
+                    document.body.removeChild(link);
+                }, 1000);
+            });
+            cardFooter.appendChild(downloadButtonElement);
+            cardFooter.appendChild(viewButtonElement);
             cardContainer.append(imageElement, cardBody, cardFooter);
             imageSection.append(cardContainer);
         })
